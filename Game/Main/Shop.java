d6761a6bdf51b4c0ab49869f92feb1be4248cad2
SmyloG <michael.m.0335@gmail.com>
2021-05-15 23:06:06 +0200

    a

--- Game/Main/Shop.java
+++ Game/Main/Shop.java
@@ -2,14 +2,13 @@ import java.util.Scanner;
 
 //building a vendor
 public class Shop {
-    static int healthPotion;
     static int money=20;
-    public static void main(String[] args){
-            shop();
-    
-        }
+
     public static void shop(){
         Scanner vendor = new Scanner(System.in);
+        int ka = 0;
+        boolean sop = ka==0 ;
+        while (sop) {
         System.out.print(   "--------------------------\n"+
                             "You have entered the Shop.\n"+
                             "--------------------------\n"+
@@ -18,20 +17,25 @@ public class Shop {
                             "1. Buy Equipment\n"+
                             "2. Buy a Healing Potion (20 G)\n"+
                             "3. Leave\n");
-        int Talk = vendor.nextInt()+1;
+        int Talk = vendor.nextInt();
 
         switch (Talk){
             case 1:
-                System.out.println("ye");
-
+                System.out.println("A new Sword or a new Armor?\n1. Sword (200 G)\n2. Armor ()");
+                
+                break;
             case 2:
             if(money>=20){
                 int heal = (int) (Math.random()*3+1);
                 System.out.println("You bought the potion! \nAnd directly drank it. How wasteful.\nYou heal " + heal + "HP.");
             } else                      {System.out.println("You don't have enough G.");}
+                money=money-20;
+                break;
             case 3:
-            System.out.println("nonye");
-        
-        }        
-    }
+            System.out.println("You leave the shop.");
+            ka ++;
+                break;
+        } ka=0;
+    }        
+}
 }
