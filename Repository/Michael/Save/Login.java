9373640f4c0a94258db79b10be11ce0c0e47d1f8
SmyloG <michael.m.0335@gmail.com>
2021-05-15 20:11:45 +0200

    a

--- Repository/Michael/Save/Login.java
+++ Repository/Michael/Save/Login.java
@@ -1,21 +1,20 @@
-package Save;
 import java.io.File;
 import java.io.IOException;
 import java.io.FileWriter;
 import java.util.Scanner;
 
 public class Login {
-static String Exp;
-static String Money;
-static String Day;
-static String userName;
+//static String Exp;
+//static String Money;
+//static String Day;
+//static String userName;
 // exp, money, day only defined for test reasons
-static int exp=200;
-static int money=2000;
-static int day=5;
+//static int exp=200;
+//static int money=2000;
+//static int day=5;
 //
 public static void main(String[] args) {
-    userName = "John"; //set userName to the name of the player
+    Game.userName = John; //set userName to the name of the player
     createFile();
     System.out.print(getSavePath()+"\n");
     writingPain();
@@ -25,48 +24,46 @@ public static void main(String[] args) {
 
 public static void createFile() {
 
-    File f = new File("res/" + userName + ".txt");
-    if (!f.createNewFile();)
-        try {
-            f.createNewFile();
-        } catch (IOException e) {
-            f.delete();
-            f.createNewFile();
-        }
+    File f = new File("res/" + Game.userName + ".txt");
+       if(f.exists()){ 
+        f.delete();
+        try {f.createNewFile(); } catch(Exception e) {System.out.println("Something went wrong while Saving");} 
+        } else 
+        {try {f.createNewFile(); } catch(Exception e) {System.out.println("Something went wrong while Saving");}}
+    }
 
-}
 
 public static String getSavePath(){
-        return "res/" + userName + ".txt";
+        return "res/" + Game.userName + ".txt";
 
 
 }
 
 public static void writingPain(){
     try{
-    FileWriter writingSave = new FileWriter("res/" + userName + ".txt");
-    String Exp = Integer.toString(exp);
-    String Money = Integer.toString(money);
-    String Day = Integer.toString(day);
-    writingSave.write(Exp +"\n"+ Money +"\n" + Day);
+    FileWriter writingSave = new FileWriter("res/" + Game.userName + ".txt");
+    String Game.Exp = Integer.toString(Game.exp);
+    String Game.Money = Integer.toString(Game.money);
+    String Game.Day = Integer.toString(Game.day);
+    writingSave.write(Game.Exp +"\n"+ Game.Money +"\n" + Game.Day);
     writingSave.close();
     System.out.println("Cheeki");
     } catch(IOException e) {System.out.println("Breeki"); e.printStackTrace();}
 }
 public static void readingPain(){
     try {
-        File f = new File("res/" + userName + ".txt");
+        File f = new File("res/" + Game.userName + ".txt");
         Scanner reader = new Scanner(f);
         while (reader.hasNextLine()) {
-            String Exp = reader.nextLine();
-            String Money = reader.nextLine();
-            String Day = reader.nextLine();
-            int exp = Integer.parseInt(Exp);
-            int money = Integer.parseInt(Money);
-            int day = Integer.parseInt(Day);
-            System.out.println("Exp: " + exp);
-            System.out.println("Money: " + money);
-            System.out.println("Day: " + day);
+            String Game.Exp = reader.nextLine();
+            String Game.Money = reader.nextLine();
+            String Game.Day = reader.nextLine();
+            int Game.exp = Integer.parseInt(Game.Exp);
+            int Game.money = Integer.parseInt(Game.Money);
+            int Game.day = Integer.parseInt(Game.Day);
+            System.out.println("Exp: " + Game.exp);
+            System.out.println("Money: " + Game.money);
+            System.out.println("Day: " + Game.day);
         }
             reader.close();
         } catch (Exception e) {}
