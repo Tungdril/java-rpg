83173ee9f465dfcb2bc96efea876f8ed504b3b3f
SmyloG <michael.m.0335@gmail.com>
2021-05-16 11:21:48 +0200

    a

--- Game/Main/createSaveFolder.java
+++ Game/Main/createSaveFolder.java
@@ -1,6 +1,6 @@
 import java.io.File;
 import java.nio.file.Path;
-
+import java.util.Scanner;
 public class createSaveFolder{
         static String userpath = System.getProperty("user.home");
         static String savepath = userpath+"/Documents/JavaRPG/saves/";     
@@ -11,4 +11,15 @@ public class createSaveFolder{
         new File(savepath).mkdirs();
         
     }
+    public static void SaveLoad(){
+        Scanner Name = new Scanner(System.in);
+        System.out.println("Please enter your Username");
+        Game.userName = Name.next();
+        System.out.println("Do you wish to \n1. create \nor \n2. load \na save file");
+        int savefile = Name.nextInt();
+        switch(savefile){
+            case 1: Save.creatingSave(); break; 
+            case 2: Load.readingPain2(); break;
+            default: System.out.print("Please use 1 or 2 the next time"); SaveLoad();
+        }}
 }
